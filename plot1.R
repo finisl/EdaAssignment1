4be603a1a798cdce3d97164e9650e2b678dcce00
Jack Lee <jack.lee@hssbc.ca>
2014-05-09 00:42:17 -0700

    Update plot 1 comments

--- plot1.R
+++ plot1.R
@@ -1,10 +1,18 @@
-## Part 1: loading data
+## Part 0: Obtain the data file
+# The steps are below:
+# 1) Down the zipped data file from:
+#      https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip
+# 2) Unzip the file to get the data txt file; household_power_consumption.txt
+# 3) Ensure the data txt file is stored under the same directory as the R plotting scripts
 
+
+
+## Part 1: loading data
 # Figure out the calculation to get only the required dates: 2007-02-01 and 2007-02-02 
 # The steps are:
-#   1) Load 1 row to figure out how many columns
-#   2) Load only the 1st column for dates
-#   3) Calcualte the beginning rows to skip & the number of rows to load
+# 1) Load 1 row to figure out how many columns
+# 2) Load only the 1st column for dates
+# 3) Calcualte the beginning rows to skip & the number of rows to load
 hpower1row <- read.table("household_power_consumption.txt", header=TRUE, sep=";", nrows=1)
 hpower1col <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", 
                          colClasses = c(NA, rep("NULL", ncol(hpower1row) - 1)))
@@ -12,14 +20,13 @@ hpower1col[,1] <- as.Date(hpower1col[,1], format="%d/%m/%Y")
 load_skip <- which.max(hpower1col$Date == "2007-02-01") - 1
 load_nrow <- which.max(hpower1col$Date == "2007-02-03") - which.max(hpower1col$Date == "2007-02-01")
 
-
-
 # Load the file with only the required dates
 hpower <- read.table("household_power_consumption.txt", header=TRUE, sep=";", skip=load_skip, nrows=load_nrow)
 names(hpower) <- names(hpower1row)
 hpower[,1] <- as.Date(hpower[,1], format="%d/%m/%Y")
 hpower[,2]  <- as.POSIXct(hpower[,2], format="%H:%M:%S")
 hpower[,2]  <- as.numeric(hpower[,2]  - trunc(hpower[,2] , units="days"))
+
 # clean up the temporary variables used for calclating the time frame
 rm(hpower1row)
 rm(hpower1col)
@@ -32,4 +39,8 @@ rm(load_nrow)
 png("plot1.png", width = 480, height = 480)
 hist(as.numeric(hpower$Global_active_power), col="red", main = "Global Active Power",xlab="Global Active Power (kilowatts)")
 dev.off()
+
+
+
+
 
